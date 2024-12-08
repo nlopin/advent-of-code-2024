f6687fd82a051d16bbb58292dd7a795484790bb0
Nikolai Lopin <code@lopin.me>
2024-12-08 21:29:33 +0100

    add day 7.1

---
+++ 7/1.js
@@ -0,0 +1,34 @@
+const { open } = require('node:fs/promises');
+
+(async () => {
+    const sum = (a,b) => a + b
+    const mult = (a,b) => a * b
+    const operations = [sum, mult]
+    const file = await open('./input.txt');
+    let s = 0
+    for await (const line of file.readLines()) {
+	const [rawResult, rawNumbers] = line.split(":").map(v => v.trim())
+	const result = parseInt(rawResult, 10)
+	const numbers = rawNumbers.split(" ").map(v => parseInt(v, 10)).reverse()
+
+	if (canCombine(numbers, result)) {
+	    console.log(numbers, result)
+	    s += result
+	}
+    }
+
+    function canCombine(numbers, result) {
+	if (numbers.length === 1) return numbers[0] === result;
+	const a = numbers.pop()
+	const b = numbers.pop()
+
+	for (const operation of operations) {
+	    const outcome = operation(a, b)
+	    if (canCombine([...numbers, outcome], result)) return true
+	}
+
+	return false
+    }
+
+    console.log(s)
+})();
